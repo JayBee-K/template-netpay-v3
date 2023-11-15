0d1da836a64025afe3f57112906ecc5bed6c0a39
Hoàng Tấn <killerdrug007@gmail.com>
2023-11-15 17:04:56 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -206,19 +206,27 @@ const handleSlideFeatured = function () {
             spaceBetween: 16,
             breakpoints: {
                 320: {
-                    slidesPerView: 2,
+                    slidesPerView: 1.25,
                     spaceBetween: 10,
                     grid: {
-                        rows: 2,
-                        fill: "row",
+                        rows: 1,
+                        fill: "column",
                     },
                 },
-                991: {
-                    slidesPerView: 3,
+                575: {
+                    slidesPerView: 1.5,
                     spaceBetween: 10,
                     grid: {
-                        rows: 2,
-                        fill: "row",
+	                    rows: 1,
+	                    fill: "column",
+                    },
+                },
+                768: {
+                    slidesPerView: 1.5,
+                    spaceBetween: 10,
+                    grid: {
+	                    rows: 2,
+	                    fill: "row",
                     },
                 },
                 1200: {
